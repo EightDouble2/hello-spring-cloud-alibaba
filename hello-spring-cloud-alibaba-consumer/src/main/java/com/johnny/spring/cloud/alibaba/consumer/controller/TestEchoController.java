4c7f336bb684ed793fad85393b01ef610319f45b
吴昊 <johnny173862903@gmail.com>
2020-01-29 02:26:52 +0800

    nacos 配置中心

--- hello-spring-cloud-alibaba-consumer/src/main/java/com/johnny/spring/cloud/alibaba/consumer/controller/TestEchoController.java
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/johnny/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -2,19 +2,30 @@ package com.johnny.spring.cloud.alibaba.consumer.controller;
 
 import com.johnny.spring.cloud.alibaba.consumer.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestEchoController {
 
     @Autowired
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping(value = "/feign/echo/{str}")
     public String echo(@PathVariable("str") String str) {
         return echoService.echo(str);
+    }
+
+    @GetMapping(value = "/feign/echo")
+    public String echo() {
+        return echoService.echo(username);
     }
 
     @GetMapping(value = "/port")
