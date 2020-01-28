aad37a45da11f7db94b50a9e39de98e1fdd9d508
吴昊 <johnny173862903@gmail.com>
2020-01-29 01:28:13 +0800

    nacos

---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/johnny/spring/cloud/alibaba/consumer/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.johnny.spring.cloud.alibaba.consumer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+    private final RestTemplate restTemplate;
+
+    public TestController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping(value = "/echo/{str}")
+    public String echo(@PathVariable("str") String str) {
+        return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
+    }
+}
